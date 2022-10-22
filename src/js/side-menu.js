1d51161e49523f07686cd73adfcb996fe6009af2
Alex Chekirda <alexchekirda@gmail.com>
2022-10-22 17:19:58 +0300

    Third commit
    Changes added

--- src/js/side-menu.js
+++ src/js/side-menu.js
@@ -1,14 +1,13 @@
-const navBarMenuBtn = document.querySelector('.nav-bar__round-menu');
-const closeBtn = document.querySelector('.side-header__round-close');
+const navBarMenuBtn = document.querySelector('.nav-bar__button-menu');
+const closeBtn = document.querySelector('.side-header__button-close');
 const sideMenu = document.querySelector('.side-menu');
 
 navBarMenuBtn.addEventListener('click', function() {
-  sideMenu.classList.remove('side-menu--hidden');
-  sideMenu.classList.add('side-menu--visible');
+    sideMenu.classList.remove('side-menu--hidden');
+    sideMenu.classList.add('side-menu--visible');
 })
 
 closeBtn.addEventListener('click', function() {
-  sideMenu.classList.remove('side-menu--visible');
-  sideMenu.classList.add('side-menu--hidden');
+    sideMenu.classList.remove('side-menu--visible');
+    sideMenu.classList.add('side-menu--hidden');
 })
-
