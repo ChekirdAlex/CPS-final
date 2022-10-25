eec323b5a04b26652238f03536aa5c0b598a914f
Alex Chekirda <alexchekirda@gmail.com>
2022-10-25 19:04:17 +0300

    Changes 1. Gradient was added 2. Titles were corrected 3. Side-menu corrected. It has fixed width. 4. Modal form has  side shadow 5. <br> tags were deleted. 6. Attributes name were added 7. console.log was deleted. 8. Blur was corrected 9. querySelector was corrected

--- src/js/side-menu.js
+++ src/js/side-menu.js
@@ -3,31 +3,31 @@ if (window.innerWidth < 1440) {
 const navBarMenuBtn = document.querySelector('.nav-bar__button-menu');
 const closeBtn = document.querySelector('.side-header__button-close');
 const sideMenu = document.querySelector('.side-menu');
-const main = document.querySelector('.main');
+const blurScreen = document.querySelector('.blur-screen');
 
-navBarMenuBtn.addEventListener("click", function (e) {
-    e.stopPropagation();
+navBarMenuBtn.addEventListener("click", function (evt) {
+    evt.stopPropagation();
     sideMenu.classList.remove('side-menu--hidden');
     sideMenu.classList.add('side-menu--visible');
-    main.classList.add('main--blurred');
+    blurScreen.classList.add('blur-screen--visible');
 });
 
-closeBtn.addEventListener("click", function (e) {
-    e.stopPropagation();
+closeBtn.addEventListener("click", function (evt) {
+    evt.stopPropagation();
     sideMenu.classList.remove('side-menu--visible');
     sideMenu.classList.add('side-menu--hidden');
-    main.classList.remove('main--blurred');
+    blurScreen.classList.remove('blur-screen--visible');
 });
 
-document.addEventListener("click", function (e) {
-    const target = e.target;
-    const its_sideMenu = target == sideMenu || sideMenu.contains(target);
+document.addEventListener("click", function (evt) {
+    const target = evt.target;
+    const its_sideMenu = target === sideMenu || sideMenu.contains(target);
     const menu_is_active = sideMenu.classList.contains("side-menu--visible");
 
     if (!its_sideMenu && menu_is_active) {
         sideMenu.classList.remove('side-menu--visible');
         sideMenu.classList.add('side-menu--hidden');
-        main.classList.remove('main--blurred');
+        blurScreen.classList.remove('blur-screen--visible');
     }
 });
 }
